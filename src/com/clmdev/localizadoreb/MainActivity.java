657536759bde43b522f7b647a71802cbd8977171
mroque <mpc4mirl@gmail.com>
2014-11-11 17:48:34 -0500

    Mapa

--- src/com/clmdev/localizadoreb/MainActivity.java
+++ src/com/clmdev/localizadoreb/MainActivity.java
@@ -1,9 +1,12 @@
 package com.clmdev.localizadoreb;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +15,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button1).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {				
+				Intent i = new Intent(getApplicationContext(),
+						Mapa.class);
+				startActivity(i);
+			}
+		});
     }
 
 
